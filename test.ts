7f991a9cea2d70082f91d61f67e2a3b06e1f5d35
Sonny <sonny.lazuardi@grabtaxi.com>
2023-11-27 11:49:38 +0000

    add test

--- test.ts
+++ test.ts
@@ -7,10 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello, Bun!');
 });
 
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+
 createServer({
   cert: Bun.file(process.env.BUN_CERT),
   key: Bun.file(process.env.BUN_KEY),
-}, app).listen(443, 'localhost', () => {
-  console.log(`Listening on port 443...`);
+}, app).listen(HTTPS_PORT, () => {
+  console.log(`Listening on port ${HTTPS_PORT}...`);
 });
 
