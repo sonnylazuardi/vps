db825e6b7fac2d6e21a7cdf72a8a42418c69fbad
Sonny <sonny.lazuardi@grabtaxi.com>
2023-11-27 14:15:16 +0000

    update orm

--- https.ts
+++ https.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import { createServer } from 'https';
-import { deleteUser, fetchUser, fetchUsers, insertUser } from "./src/db";
+import { deleteUser, fetchUser, fetchUsers, insertUser, updateUser } from "./src/db";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send(`Welcome to Bun over HTTPS! Requested path: ${req.url}! SHIP IT V11 🚀`);
+  res.send(`Welcome to Bun over HTTPS! Requested path: ${req.url}! SHIP IT V12 🚀`);
 });
 
 app.post('/deploy', (req, res) => {
@@ -20,23 +20,28 @@ app.post('/deploy', (req, res) => {
 });
 
 app.post('/users', async (req, res) => {
-  const result = insertUser({ name: req.body.name, email: req.body.email });
+  const result = await insertUser({ name: req.body.name, email: req.body.email });
   res.json(result);
 });
 
 app.delete('/users/:id', async (req, res) => {
-  const result = deleteUser(req.params.id);
+  const result = await deleteUser(req.params.id);
   res.json(result);
 });
 
 app.get('/users/:id', async (req, res) => {
-  const user = await fetchUser(req.params.id);
-  res.json(user);
+  const result = await fetchUser(req.params.id);
+  res.json(result);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const result = await updateUser(req.params.id, req.body);
+  res.json(result);
 });
 
 app.get('/users', async (req, res) => {
-  const users = await fetchUsers();
-  res.json(users);
+  const result = await fetchUsers();
+  res.json(result);
 });
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 443;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,23 +1,32 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import { InsertUser, User, users } from './schema';
+import * as schema from './schema';
 import { eq } from 'drizzle-orm';
 
 const sqlite = new Database('sqlite.db');
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
 
 export const fetchUsers = async () => {
-  const result: User[] = db.select().from(users).all();
+  const result = await db.query.users.findMany();
   return result;
 }
 
 export const fetchUser = async (id: number) => {
-  const result = db.select().from(users).where(eq(users.id, id)).all().at(0) || null;
+  const result = await db.query.users.findMany({ where: eq(users.id, id) });
+  return result[0];
+}
+
+export const updateUser = async (id: number, body: User) => {
+  const result = await db.update(users)
+    .set(body)
+    .where(eq(users.id, id)).returning();
   return result;
 }
 
 export const insertUser = async (user: InsertUser) => {
-  return db.insert(users).values(user).returning();
+  const result = await db.insert(users).values(user).returning();
+  return result;
 }
 
 export const deleteUser = async (id: number) => {
